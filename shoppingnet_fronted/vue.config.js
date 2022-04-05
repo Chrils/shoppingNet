01e1d3d09ed49e285cb679821de2ba5204905105
chrils <2991968248@qq.com>
2022-04-05 16:22:27 +0800

    初始化项目

--- shoppingnet_fronted/vue.config.js
+++ shoppingnet_fronted/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'components': '@/components',
+        'content': 'components/content',
+        'common': 'components/common',
+        'assets': '@/assets',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
 })
